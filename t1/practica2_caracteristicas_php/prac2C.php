960891bd9e1f89d962bb0234c303f18506f7aa31
cesar <cg.gtav@gmail.com>
2024-08-09 14:27:40 +0200

    pac2C.php

--- t1/practica2_caracteristicas_php/prac2C.php
+++ t1/practica2_caracteristicas_php/prac2C.php
@@ -7,22 +7,31 @@
 </head>
 <body>
     <?php
-    $pucharse = 501;
+
+    function calcDiscount($pucharse) {
 
     if($pucharse < 100){
+        print "el precio incial es de $pucharse" . "<br>";
         print "tienes que llegar a  100 para que se aplique un descuento";
-        return;
     }
 
     if($pucharse >= 100 && $pucharse <= 500){
-        print "se te aplica un descuento del 10%";
-        return;
+        print "el precio incial es de $pucharse" . "<br>";
+         $discount = $pucharse * 10 /100;
+        print "se te aplica un descuento del 10%" . "<br>";
+        print "la cantidad a pagar es " . $pucharse - $discount;
     }
 
     if($pucharse > 500){
-        print "se te aplica un descuento del 15%";
-        return;
+        print "el precio incial es de $pucharse" . "<br>";
+        $discount = $pucharse * 15 /100;
+        print "se te aplica un descuento del 15%" . "<br>";
+        print "la cantidad a pagar es " . $pucharse - $discount;
     }
+    }  
+    
+    calcDiscount(99);
+
     ?>
 </body>
 </html>
